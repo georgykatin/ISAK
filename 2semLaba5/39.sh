f62dc97eeaaf31363774c1d10905cdbb2fe76acb
Georgy <96377078+georgykatin@users.noreply.github.com>
2022-06-14 01:14:24 +0300

    Create 39.sh

---
+++ 2semLaba5/39.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while read a ; do
+sleep 5 ; 
+echo "$a" ; 
+sleep 5 ;
+date ; 
+done >"$2" <"$1" & pa=$! ; 
+while sleep 7 ; do 
+cat "$2" ; done & pb=$! ;
+while read x ; [ m"$x" != mquit ] ; do : ;
+done ;
+kill $pa ; kill $pb ; rm "$2"
